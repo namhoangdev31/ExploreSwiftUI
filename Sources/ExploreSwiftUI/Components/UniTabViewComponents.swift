120418428465dcdcc7bbb88fc95f2af968afd4d8
hoangnam <nguyenhoangnam31082000@gmail.com>
2026-07-04 12:01:13 +0700

    fix: correct line break in tabViewSearchActivation modifier call

--- Sources/ExploreSwiftUI/Components/UniTabViewComponents.swift
+++ Sources/ExploreSwiftUI/Components/UniTabViewComponents.swift
@@ -131,7 +131,8 @@ public struct UniTabView<Selection: Hashable>: View {
         return Group {
             if #available(iOS 26.0, macOS 26.0, tvOS 26.0, watchOS 26.0, visionOS 26.0, *) {
                 if hasSearchTab {
-                    tabView.tabViewSearchActivation(.searchTabSelection)
+                    tabView.tabViewSearchAc
+                    tivation(.searchTabSelection)
                 } else {
                     tabView
                 }
